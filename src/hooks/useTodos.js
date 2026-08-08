6cb0f51073dcaa18b529d29b9a9c71562ea2129d
MM-MatthewSoriano <matthewsoriano763@gmail.com>
2026-08-08 13:19:21 +0800

    chore: fixed the delete todo function

--- src/hooks/useTodos.js
+++ src/hooks/useTodos.js
@@ -1,6 +1,6 @@
 import { useMemo, useEffect, useState, useCallback, useReducer } from 'react';
-// import useLocalStorage from "./useLocalStorage.js";
 
+// -----Functions-----
 function getInitialTodos() {
     const savedTodos = localStorage.getItem("todos");
 
@@ -18,24 +18,10 @@ function useTodos() {
     // It manages the timer state and provides functions to control the timer.
 
     // -----Use States-----
-    // Moved to useLocalStorage.js
-    // // Things that react needs to remember
-    // const [todos, setTodos] = useLocalStorage("todos",[]);
     const [todos, dispatch] = useReducer(todosReducer, [], getInitialTodos);
-
-    //     // Lazy Initializer: React only runs that function once, when creating the state.
-    //     // Typically needed when computing the initial state SYNCHRONOUSLY
-    //     () => { 
-    //         return JSON.parse(localStorage.getItem("todos")) || [];
-    //     }
-    // ); 
     const [filter, setFilter] = useState("all");
     const [searchTitle, setSearchTitle] = useState("");
     const [sortBy, setSortBy] = useState("newest");
-
-    // These states are only typically needed for loading data ASYNCHRONOUSLY
-    // const [loading, setLoading] = useState(true); // when the component first mounts, show a loading state, set loading to true
-    // const [error, setError] = useState(null); // check for errors when fetching data, set error to null
 
     // -----Derived States-----
     // Derived states are things react can calculate
@@ -103,28 +89,6 @@ function useTodos() {
     }, [todos, filter, searchTitle, sortBy]);
    
     // -----Functions-----
-    // Add a new todo to the list of todos
-    // newTodo is the title of the new todo to be added
-    // function addNewTodo(newTodo) { 
-
-    //     // setTodos is called to update the todos state with a new array that includes the previous todos and the new todo
-    //     // inside setTodos, we use a callback function that takes the previous todos as an argument (prevTodos) and returns 
-    //     // a new array that includes all the previous todos and the new todo object.
-    //     setTodos(prevTodos => [ 
-
-    //         // spread operator is used to copy the previous todos into the new array
-    //         ...prevTodos,
-
-    //         // New todo object with a unique id, title, and completed status
-    //         {
-    //             id: Date.now(),
-    //             title: newTodo,
-    //             completed: false
-    //         }
-
-    //     ]);
-    // }
-
     // useReducer
     function todosReducer(todos, action) {
 
@@ -193,103 +157,6 @@ function useTodos() {
         }
     }
 
-    //     // Check if the action type is DELETE_TODO
-    //     if (action.type === "DELETE_TODO") {
-
-    //         // Create a new array by looking at each todo one by one.
-    //         // Keep every todo whose id does not match the id passed through action.payload.
-    //         return todos.filter(todo => todo.id !== action.payload);
-    //     }
-
-    //     // Check if the action type is ADD_TODO
-    //     if (action.type === "ADD_TODO") {
-
-    //         // Create a new array by copying all existing todos.
-    //         // Add the new todo object passed through action.payload to the end of the array.
-    //         return [
-    //             ...todos,
-    //             action.payload
-    //         ];
-    //     }
-
-    //     // Check if the action type is TOGGLE_TODO
-    //     if (action.type === "TOGGLE_TODO") {
-
-    //         // Create a new array by looking at each todo one by one.
-    //         return todos.map(todo => {
-
-    //             // "Is this the todo the user clicked?"
-    //             // action.payload contains the id of the todo that needs to be toggled.
-    //             if (todo.id === action.payload) {
-
-    //                 // Create a new object that copies all the existing properties.
-    //                 // Overwrite only the completed property.
-    //                 // The ! operator changes true to false and false to true.
-    //                 return {
-    //                     ...todo,
-    //                     completed: !todo.completed
-    //                 };
-    //             }
-
-    //             // If this isn't the todo we're updating, leave it unchanged.
-    //             return todo;
-    //         });
-    //     }
-
-    //     // Check if the action type is UPDATE_TODO
-    //     if (action.type === "UPDATE_TODO") {
-
-    //         // Create a new array by looking at each todo one by one.
-    //         return todos.map(todo => {
-
-    //             // "Is this the todo the user clicked?"
-    //             // action.payload.id contains the id of the todo that needs to be updated.
-    //             if (todo.id === action.payload.id) {
-
-    //                 // Create a new object that copies all the existing properties.
-    //                 // Overwrite only the title property with the new title.
-    //                 return {
-    //                     ...todo,
-    //                     title: action.payload.newTitle
-    //                 };
-    //             }
-
-    //             // If this isn't the todo we're updating, leave it unchanged.
-    //             return todo;
-    //         });
-    //     }
-
-    //     // Check if the action type is LOAD_TODOS
-    //     if (action.type === "LOAD_TODOS") {
-
-    //         // Replace the current todos with the todos loaded from localStorage.
-    //         return action.payload;
-    //     }
-
-    //     // Return todos if none of the action types matched.
-    //     return todos;
-    // }
-
-    // const addNewTodo = useCallback((newTodo) => {
-
-    //     // setTodos is called to update the todos state with a new array that includes the previous todos and the new todo
-    //     // inside setTodos, we use a callback function that takes the previous todos as an argument (prevTodos) and returns 
-    //     // a new array that includes all the previous todos and the new todo object.
-    //     setTodos(prevTodos => [ 
-
-    //         // spread operator is used to copy the previous todos into the new array
-    //         ...prevTodos,
-
-    //         // New todo object with a unique id, title, and completed status
-    //         {
-    //             id: Date.now(),
-    //             title: newTodo,
-    //             completed: false
-    //         }
-
-    //     ]);
-    // }, [setTodos]);
-
     // useReducer
     function addNewTodo(newTodo) {
         dispatch({
@@ -302,85 +169,13 @@ function useTodos() {
         });
     }
 
-    // Delete a todo from the list of todos
-    // the id of the todo to be deleted is passed as an argument from the 
-    // ShowTodos component when the delete button is clicked
-    // function deleteTodo(id) {
-
-    //     // React gives you the latest array of todos.
-    //     setTodos(prevTodos => {
-    //         return prevTodos.filter(todo => todo.id !== id);
-    //     }) 
-    // }
-
-    // useMemo     → remembers a calculated VALUE
-    // useCallback → remembers a FUNCTION
-    // React.memo  → memoizes a COMPONENT
-
-    // const deleteTodo = useCallback((id) => {
-    //     setTodos(prevTodos => {
-    //         return prevTodos.filter(todo => todo.id !== id);
-    //     }) 
-    // },[setTodos])
-
     // Using the useReducer function
-    const deleteTodo = (id) => {
+    function deleteTodo(id) {
         dispatch({
             type: "DELETE_TODO",
             payload: id
         })
     }
-
-    // Toggle the completed status of a todo
-    // the id of the todo to be toggled is passed as an argument from the 
-    // ShowTodos component when the checkbox is clicked
-    // function toggleTodo(id) {
-
-    //     // React gives you the latest array of todos.
-    //     setTodos(prevTodos => { 
-
-    //         // Create a new array by looking at each todo one by one.
-    //         return prevTodos.map(todo => {
-
-    //             // "Is this the todo the user clicked?"
-    //             if (todo.id === id) {
-
-    //                 // Create a new object that copies all the existing properties.
-    //                 // Overwrite only the completed property.   
-    //                 return {
-    //                     ...todo,
-    //                     completed: !todo.completed
-    //                 }
-    //             }
-
-    //             // If this isn't the todo we're updating, leave it unchanged.
-    //             return todo;
-    //         })
-    //     })
-    // }
-
-    // const toggleTodo = useCallback((id) => {
-    //     setTodos(prevTodos => { 
-
-    //         // Create a new array by looking at each todo one by one.
-    //         return prevTodos.map(todo => {
-
-    //             // "Is this the todo the user clicked?"
-    //             if (todo.id === id) {
-
-    //                 // Create a new object that copies all the existing properties.
-    //                 // Overwrite only the completed property.   
-    //                 return {
-    //                     ...todo,
-    //                     completed: !todo.completed
-    //                 }
-    //             }
-
-    //             // If this isn't the todo we're updating, leave it unchanged.
-    //             return todo;
-    //         })
-    //     })
-    // }, [setTodos])
 
     // useReducer
     function toggleTodo(id) {
@@ -389,57 +184,6 @@ function useTodos() {
             payload: id
         })
     }
-
-    // Update todo
-    // function updateTodo(id, newTitle) {
-
-    //     // React gives you the latest array of todos.
-    //     setTodos(prevTodos => { 
-
-    //         // Create a new array by looking at each todo one by one.
-    //         return prevTodos.map(todo => {
-
-    //             // "Is this the todo the user clicked?"
-    //             if (todo.id === id) {
-
-    //                 // Create a new object that copies all the existing properties.
-    //                 // Overwrite only the completed property.   
-    //                 return {
-    //                     ...todo,
-    //                     title: newTitle
-    //                 }
-    //             }
-
-    //             // If this isn't the todo we're updating, leave it unchanged.
-    //             return todo;
-    //         })
-    //     })
-    // }
-
-    // const updateTodo = useCallback((id, newTitle) => {
-
-    //     // React gives you the latest array of todos.
-    //     setTodos(prevTodos => { 
-
-    //         // Create a new array by looking at each todo one by one.
-    //         return prevTodos.map(todo => {
-
-    //             // "Is this the todo the user clicked?"
-    //             if (todo.id === id) {
-
-    //                 // Create a new object that copies all the existing properties.
-    //                 // Overwrite only the completed property.   
-    //                 return {
-    //                     ...todo,
-    //                     title: newTitle
-    //                 }
-    //             }
-
-    //             // If this isn't the todo we're updating, leave it unchanged.
-    //             return todo;
-    //         })
-    //     })
-    // }, [setTodos])
 
     function updateTodo(id, newTitle) {
         dispatch({
@@ -466,80 +210,14 @@ function useTodos() {
         console.log("Saving:", todos)
         localStorage.setItem("todos", JSON.stringify(todos));
     }, [todos]);
-
-    // // Loading todos from local storage
-    // useEffect(() => {
-    //     const savedTodos = JSON.parse(
-    //         localStorage.getItem("todos")
-    //     ) || [];
-    //     setTodos(savedTodos);
-    //     setLoading(false)
-    // }, []);
-
-    // useEffect(() => {
-    //     const savedTodos = localStorage.getItem("todos");
-
-    //     if (savedTodos !== null) {
-
-    //         const parsedTodos = JSON.parse(savedTodos);
-
-    //         dispatch({
-    //             type: "LOAD_TODOS",
-    //             payload: parsedTodos
-    //         })
-
-    //     }
-    // }, []);
-
     
     // Use effect for logging sort changes
     useEffect(() => {
     console.log("Sorted todos changed:", filteredTodos);
     }, [filteredTodos]);
 
-
-    // // Fetch data from the API when the component mounts
-    // useEffect(() => {
-
-    //     // function fetchTodos is an async function that fetches data from the API
-    //     async function fetchTodos() {
-
-    //         // try block is used to catch any errors that occur during the fetch operation
-    //         try {
-                
-    //             // Send GET request, wait for server response.
-    //             const response = await fetch('https://jsonplaceholder.typicode.com/todos'); 
-
-    //             // Ensure HTTP failures, Network Failures end in same catch block
-    //             if (!response.ok) {
-    //                 throw new Error('Failed to fetch todos.');
-    //             }
-
-    //             const data = await response.json(); // Convert the response to JSON
-    //             console.log(data); // Log the data to the console
-    //             setTodos(data); // Update the todos state with the fetched data
-    //         }
-
-    //         // catch block is used to handle any errors that occur during the fetch operation
-    //         catch (error) {
-    //             setError(error); // Update the error state with the error that occurred
-    //         }
-
-    //         // finally block is used to execute code after the try and catch blocks, regardless of whether an error occurred or not
-    //         finally {
-    //             setLoading(false); // Set loading to false after data is fetched  
-    //         }
-  
-    //     }
-    //     fetchTodos(); // Call the async function to fetch todos
-
-    // }, []);
-
-    // Return everything that the component needs to know about the todos state
     return {
         todos,
-        // loading,
-        // error,
         searchTitle,
         setSearchTitle,
         filteredTodos,
